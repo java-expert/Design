de81da1470056c70d22c9016fcbba580a7823b64
java-expert <jagjotmca@yahoo.co.in>
2016-09-17 17:22:19 +0530

    prototype

--- src/creational_prototype/Employee.java
+++ src/creational_prototype/Employee.java
@@ -1,5 +1,37 @@
 package creational_prototype;
 
-public class Employee {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Employee implements Cloneable{
+	
+	private List<String> empList;
+	
+	public Employee(){
+		empList=new ArrayList<String>();
+	}
+	
+	public List<String> getEmpList() {
+		return empList;
+	}
+
+	public void loadEmp(){
+		empList.add("emp1");
+		empList.add("emp2");
+		
+	}
+	
+	public Employee(List<String> empList){
+		this.empList=empList;
+		
+	}
+	public Employee clone(){
+		List<String> temp=new ArrayList<String>();
+		for(String s:empList){
+			temp.add(s);
+		}
+		return new Employee(temp);
+		
+	}
+	
 }
---
+++ src/creational_prototype/MainTest.java
@@ -0,0 +1,14 @@
+package creational_prototype;
+
+public class MainTest {
+	public static void main(String[] args) {
+		Employee e1=new Employee();
+		e1.loadEmp();
+		
+		Employee e2=e1.clone();
+		
+		System.out.println(e1.getEmpList());
+		System.out.println(e2.getEmpList());
+	}
+
+}
