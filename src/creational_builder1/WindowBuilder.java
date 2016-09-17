33d4f9219eac34cf76ec12c461042eea6d7f939c
java-expert <jagjotmca@yahoo.co.in>
2016-09-17 15:58:55 +0530

    builder

--- src/creational_builder1/WindowBuilder.java
+++ src/creational_builder1/WindowBuilder.java
@@ -3,6 +3,7 @@ package creational_builder1;
 public class WindowBuilder {
 	
 	public static Window createWindow(){
+		System.out.println("sdk test");
 		Toolbar toolbar=new Toolbar();
 		Menu menu=new Menu();
 		Window w=new Window();
